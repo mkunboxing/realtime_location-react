f1c250eb5f6d65ab85fc213b9bee18f0f8e6061c
Mukul Kumar <mkunboxing@gmail.com>
2024-07-17 22:23:54 +0530

    pushed

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
-// server.js
+// backend/server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors'); // Import cors
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000", // Allow your frontend URL
+    methods: ["GET", "POST"]
+  }
+});
+
 const port = 4000;
 
-const cors = require('cors');
-app.use(cors());
+// Use cors middleware
+app.use(cors({
+  origin: "http://localhost:3000" // Allow your frontend URL
+}));
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
 io.on('connection', (socket) => {
   console.log('A user connected');
 
